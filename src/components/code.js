64d0da8fd9b1d5e48a7a44dddfac2c7ec3581beb
Jon Linnell <linnell.jon@gmail.com>
2020-09-26 23:39:10 +0100

    Monokai colour theme!

--- src/components/code.js
+++ src/components/code.js
@@ -1,21 +1,31 @@
 /* eslint-disable react/jsx-key */
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
+import theme from '../utils/code-theme';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
 export const Code = ({ codeString, language, ...props }) =>
   props['react-live']
     ? (
-      <LiveProvider code={codeString} noInline={true}>
+      <LiveProvider
+        code={codeString}
+        noInline={true}
+        theme={theme}
+      >
         <LiveEditor />
         <LiveError />
         <LivePreview />
       </LiveProvider>
     )
     : (
-      <Highlight {...defaultProps} code={codeString} language={language}>
+      <Highlight
+        {...defaultProps}
+        theme={theme}
+        code={codeString}
+        language={language}
+      >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
+          <pre className={className} style={{ ...style, padding: '0.5rem' }}>
             {tokens.map((line, i) => (
               <div {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
---
+++ src/utils/code-theme.js
@@ -0,0 +1,99 @@
+const theme = {
+  plain: {
+    color: "#f7f1ff",
+    backgroundColor: "#363537"
+  },
+  styles: [
+    {
+      types: ["prolog"],
+      style: {
+        color: "rgb(0, 0, 128)"
+      }
+    },
+    {
+      types: ["comment", "block-comment"],
+      style: {
+        color: "#69676c"
+      }
+    },
+    {
+      types: ["builtin", "changed", "keyword"],
+      style: {
+        color: "#5ad4e6"
+      }
+    },
+    {
+      types: ["number", "inserted", "constant", "boolean"],
+      style: {
+        color: "#948ae3"
+      }
+    },
+    {
+      types: ["attr-name", "variable"],
+      style: {
+        color: "#5ad4e6"
+      }
+    },
+    {
+      types: ["deleted", "string", "attr-value"],
+      style: {
+        color: "#fce566"
+      }
+    },
+    {
+      types: ["selector"],
+      style: {
+        color: "#fce566"
+      }
+    },
+    {
+      // Fix tag color
+      types: ["tag"],
+      style: {
+        color: "#fc618d"
+      }
+    },
+    {
+      // Fix tag color for HTML
+      types: ["tag"],
+      languages: ["markup"],
+      style: {
+        color: "#fdf9f3"
+      }
+    },
+    {
+      types: ["operator"],
+      style: {
+        color: "#fc618d"
+      }
+    },
+    {
+      // Fix punctuation color for HTML
+      types: ["punctuation"],
+      languages: ["markup"],
+      style: {
+        color: "#fdf9f3"
+      }
+    },
+    {
+      types: ["function"],
+      style: {
+        color: "#7bd88f"
+      }
+    },
+    {
+      types: ["class-name"],
+      style: {
+        color: "#5ad4e6"
+      }
+    },
+    {
+      types: ["parameter"],
+      style: {
+        color: "#fd9353"
+      }
+    },
+  ]
+};
+
+export default theme;
