d8a8eeef22d65d1cd33b9d779ad3570d9e4b6fca
Jon Linnell <linnell.jon@gmail.com>
2020-09-26 18:43:34 +0100

    Update styles and styling

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,17 @@
+export const LARGER_DISPLAY_WIDTH = '1600px';
+export const LARGE_DISPLAY_WIDTH = '1280px';
+export const DEFAULT_WIDTH = '980px';
+export const TABLET_WIDTH = '768px';
+export const MOBILE_WIDTH = '480px';
+
+export const LARGER_DISPLAY_MEDIA_QUERY = `@media only screen and (max-width:${LARGER_DISPLAY_WIDTH})`;
+export const LARGE_DISPLAY_MEDIA_QUERY = `@media only screen and (max-width:${LARGE_DISPLAY_WIDTH})`;
+export const DEFAULT_MEDIA_QUERY = `@media only screen and (max-width:${DEFAULT_WIDTH})`;
+export const TABLET_MEDIA_QUERY = `@media only screen and (max-width:${TABLET_WIDTH})`;
+export const MOBILE_MEDIA_QUERY = `@media only screen and (max-width:${MOBILE_WIDTH})`;
+
+export const MIN_LARGER_DISPLAY_MEDIA_QUERY = `@media (min-width:${LARGER_DISPLAY_MEDIA_QUERY})`;
+export const MIN_LARGE_DISPLAY_MEDIA_QUERY = `@media (min-width:${LARGER_DISPLAY_WIDTH})`;
+export const MIN_DEFAULT_MEDIA_QUERY = `@media (min-width:${DEFAULT_WIDTH})`;
+export const MIN_TABLET_MEDIA_QUERY = `@media (min-width:${TABLET_WIDTH})`;
+export const MIN_MOBILE_MEDIA_QUERY = `@media (min-width:${MOBILE_WIDTH})`;
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,109 @@
 import Typography from 'typography';
-import Wordpress2016 from 'typography-theme-wordpress-2016';
+import { MOBILE_MEDIA_QUERY } from './breakpoints';
+import {
+  FONT_FACE_SERIF,
+  FONT_FACE_SANS_SERIF,
+  FONT_FACE_MONOSPACE,
+} from './design';
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    'a.gatsby-resp-image-link': {
+const theme = {
+  title: "Jon's Crappy Blog Theme",
+  baseFontSize: '18px',
+  baseLineHeight: 1.66,
+  scaleRatio: 5 / 2,
+  googleFonts: [
+    {
+      name: FONT_FACE_SANS_SERIF,
+      styles: ['100', '200'],
+    },
+    {
+      name: FONT_FACE_SERIF,
+      styles: ['400', '400i', '700', '700i', '900', '900i'],
+    },
+    {
+      name: FONT_FACE_MONOSPACE,
+      styles: ['400'],
+    },
+  ],
+  headerFontFamily: [FONT_FACE_SANS_SERIF, 'Georgia', 'serif'],
+  bodyFontFamily: [FONT_FACE_SERIF, 'Georgia', 'serif'],
+  bodyColor: 'rgb(50, 50, 50)',
+  headerWeight: 200,
+  bodyWeight: 300,
+  boldWeight: 400,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    h1: {
+      fontFamily: [FONT_FACE_SANS_SERIF, 'sans-serif'].join(','),
+      weight: 100,
+    },
+    blockquote: {
+      ...scale(1 / 6),
+      color: 'rgb(110, 110, 110)',
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13 / 16),
+      marginLeft: rhythm(-1),
+      borderLeft: `${rhythm(3 / 16)} solid rgb(100, 100, 100)`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: options.bodyColor,
+      fontWeight: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: 'disc',
+    },
+    'ul,ol': {
+      marginLeft: 0,
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      'ul,ol': {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(9 / 16),
+      },
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      marginTop: rhythm(2),
+    },
+    h4: {
+      letterSpacing: '0.140625em',
+      textTransform: 'uppercase',
+    },
+    h6: {
+      fontStyle: 'italic',
+    },
+    a: {
+      boxShadow: '0 1px 0 0 currentColor',
+      color: '#007acc',
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
       boxShadow: 'none',
     },
-  };
+    'mark,ins': {
+      background: '#007acc',
+      color: 'white',
+      padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
+      textDecoration: 'none',
+    },
+    code: {
+      fontFamily: [FONT_FACE_MONOSPACE, 'Courier New'].join(', '),
+      fontSize: '0.9rem',
+      backgroundColor: 'rgb(240, 240, 240)',
+    }
+  }),
 };
 
-delete Wordpress2016.googleFonts;
-
-const typography = new Typography(Wordpress2016);
+const typography = new Typography(theme);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
